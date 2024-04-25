008172a591d94f45d78533e022d83e755fd76930
Ainazik Tashtanbekova <157056073+tashtanbekova@users.noreply.github.com>
2024-04-25 19:21:47 +0300

    All DONE

--- TransfwrParamaters/main.cpp
+++ TransfwrParamaters/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int& a, int& b);
 
 void main()
 {
@@ -12,9 +12,9 @@ void main()
 	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int a, int b)
+void Exchange(int& a, int& b)
 {
-	int buffer = a;
+	int buffer =a;
 	a = b; 
 	b = buffer;
 }
